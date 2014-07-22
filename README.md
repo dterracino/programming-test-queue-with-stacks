65ec3cc97352dfc989d7971eb8a3caf30bb67eb1
Kim Pettersen <kim.a.pettersen@gmail.com>
2014-07-22 14:49:43 -0700

    Added readme and comment

---
+++ README.md
@@ -0,0 +1,2 @@
+Simple queue and Stack implementation.
+
--- queue.js
+++ queue.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// This is a simple queue implementation. I chose to implement this in a way where dequeuing the
+// queue allways takes longer time than enqueing it.
+//
+// Other ways I could do this would be to share the load between enqueue and dequeue and just not reverse
+// the queue before and after removeing an item to the queue, and check if the "enqueue-queue" or the
+// "dequeue-queue" is currently populated.
+
+
+
 var Stack = require('./stack.js');
 
 function Queue() {
